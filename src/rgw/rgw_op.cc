173d310547ea18693f2aba1f21fc18acafa6a840
Alex Reid <areid@akamai.com>
2023-11-16 11:56:28 +0000

    'validate-only' to 'x-rgw-validate-only'

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -5863,7 +5863,7 @@ void RGWPutLC::execute(optional_yield y)
     ldpp_dout(this, 15) << "New LifecycleConfiguration:" << ss.str() << dendl;
   }
 
-  auto validate_only = s->info.env->get("HTTP_VALIDATE_ONLY");
+  auto validate_only = s->info.env->get("HTTP_X_RGW_VALIDATE_ONLY");
   if (validate_only != nullptr) {
     ldpp_dout(this, 0) << "validate-only header set - exiting early" << dendl;
     return;
